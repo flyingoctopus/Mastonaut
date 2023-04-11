flyingoctopus/Mastonaut

commit 508289bb45e675220919412ab5d67aa22532623b

features/hashtag-autocomplete: mentions code is mostly back

CoreTootin/Controls/BaseComposerTextView.swift
@@ -33,7 +33,7 @@ open class BaseComposerTextView: SuggestionTextView
 
 	public override func insertNewline(_ sender: Any?)
 	{
-		guard suggestionWindowController.isWindowVisible == false else
+		guard let activeSuggestionWindowController, activeSuggestionWindowController.isWindowVisible == false else
 		{
 			insertCurrentlySelectedSuggestion()
 			return

CoreTootin/Controls/SuggestionTextView.swift
@@ -21,55 +21,61 @@ import AppKit
 
 open class SuggestionTextView: NSTextView
 {
-	public weak var accountSuggestionsProvider: AccountSuggestionTextViewSuggestionsProvider?
-	public weak var hashtagSuggestionsProvider: HashtagSuggestionTextViewSuggestionsProvider?
+	public weak var accountSuggestionsProvider: SuggestionTextViewSuggestionsProvider?
+	public weak var hashtagSuggestionsProvider: SuggestionTextViewSuggestionsProvider?
 
 	public weak var imagesProvider: AccountSuggestionWindowImagesProvider?
 	{
-		get { return suggestionWindowController.imagesProvider }
-		set { suggestionWindowController.imagesProvider = newValue }
+		get { return accountSuggestionWindowController.imagesProvider }
+		set { accountSuggestionWindowController.imagesProvider = newValue }
 	}
 
-	public private(set) lazy var suggestionWindowController = SuggestionWindowController()
+	public private(set) lazy var accountSuggestionWindowController = SuggestionWindowController(mode: .mention)
+	public private(set) lazy var hashtagSuggestionWindowController = SuggestionWindowController(mode: .hashtag)
+	
+	public var activeSuggestionWindowController : SuggestionWindowController?
 
 	private var lastSuggestionRequestId: UUID?
 
-	public override func moveUp(_ sender: Any?)
+	override public func moveUp(_ sender: Any?)
 	{
-		guard suggestionWindowController.isWindowVisible else
+		guard let activeSuggestionWindowController, activeSuggestionWindowController.isWindowVisible
+		else
 		{
 			super.moveUp(sender)
 			return
 		}
 
-		suggestionWindowController.selectPrevious(sender)
+		activeSuggestionWindowController.selectPrevious(sender)
 	}
 
-	public override func moveDown(_ sender: Any?)
+	override public func moveDown(_ sender: Any?)
 	{
-		guard suggestionWindowController.isWindowVisible else
+		guard let activeSuggestionWindowController, activeSuggestionWindowController.isWindowVisible
+		else
 		{
 			super.moveDown(sender)
 			return
 		}
 
-		suggestionWindowController.selectNext(sender)
+		activeSuggestionWindowController.selectNext(sender)
 	}
 
-	public override func cancelOperation(_ sender: Any?)
+	override public func cancelOperation(_ sender: Any?)
 	{
-		guard suggestionWindowController.isWindowVisible else
+		guard let activeSuggestionWindowController, activeSuggestionWindowController.isWindowVisible
+		else
 		{
 			return
 		}
 
 		dismissSuggestionsWindow()
 	}
-	
+
 	open func textStorage(_ textStorage: NSTextStorage,
-						  didProcessEditing editedMask: NSTextStorageEditActions,
-						  range editedRange: NSRange,
-						  changeInLength delta: Int)
+	                      didProcessEditing editedMask: NSTextStorageEditActions,
+	                      range editedRange: NSRange,
+	                      changeInLength delta: Int)
 	{
 		guard
 			undoManager?.isUndoing != true,
@@ -77,28 +83,28 @@ open class SuggestionTextView: NSTextView
 		else { return }
 
 		DispatchQueue.main.async
-			{
-				self.dispatchSuggestionsFetch()
-			}
+		{
+			self.dispatchSuggestionsFetch()
+		}
 	}
 
 	public func dispatchSuggestionsFetch()
 	{
 		SuggestionTextView.cancelPreviousPerformRequests(withTarget: self,
-														 selector: #selector(reallyDispatchSuggestionsFetch),
-														 object: nil)
+		                                                 selector: #selector(reallyDispatchSuggestionsFetch),
+		                                                 object: nil)
 
 		perform(#selector(reallyDispatchSuggestionsFetch), with: nil, afterDelay: 0.33)
 	}
 
 	public func dismissSuggestionsWindow()
 	{
-		suggestionWindowController.close()
+		activeSuggestionWindowController?.close()
 	}
 
 	public func insertCurrentlySelectedSuggestion()
 	{
-		suggestionWindowController.insertSelectedSuggestion()
+		activeSuggestionWindowController?.insertSelectedSuggestion()
 		dismissSuggestionsWindow()
 	}
 
@@ -123,11 +129,13 @@ open class SuggestionTextView: NSTextView
 
 		if mode == .mention, let provider = accountSuggestionsProvider
 		{
-		provider.suggestionTextView(self, suggestionsForQuery: mention)
-		{
-			[weak self] suggestions in
+			provider.suggestionTextView(self, suggestionsForQuery: mention)
+			{
+				[weak self] result in
 
-				guard !suggestions.isEmpty
+				guard let container = result as? [SuggestionContainer],
+					  !container.isEmpty,
+					  case SuggestionContainer.mention(_) = container[0]
 				else
 				{
 					DispatchQueue.main.async { self?.dismissSuggestionsWindow() }
@@ -137,7 +145,7 @@ open class SuggestionTextView: NSTextView
 				DispatchQueue.main.async
 				{
 					guard self?.lastSuggestionRequestId == requestId else { return }
-//					self?.showSuggestionsWindow(mode: mode, with: suggestions, mentionRange: range)
+					self?.showSuggestionsWindow(mode: mode, with: container, mentionRange: range)
 				}
 			}
 		}
@@ -145,22 +153,24 @@ open class SuggestionTextView: NSTextView
 		{
 			provider.suggestionTextView(self, suggestionsForQuery: mention)
 			{
-				[weak self] suggestions in
+				[weak self] result in
 
-				guard !suggestions.isEmpty
+				guard let container = result as? [SuggestionContainer],
+					  !container.isEmpty,
+					  case SuggestionContainer.hashtag(_) = container[0]
 				else
-			{
-				DispatchQueue.main.async { self?.dismissSuggestionsWindow() }
-				return
-			}
+				{
+					DispatchQueue.main.async { self?.dismissSuggestionsWindow() }
+					return
+				}
 
-			DispatchQueue.main.async
-			{
-				guard self?.lastSuggestionRequestId == requestId else { return }
-				self?.showSuggestionsWindow(mode: mode, with: suggestions, mentionRange: range)
+				DispatchQueue.main.async
+				{
+					guard self?.lastSuggestionRequestId == requestId else { return }
+					self?.showSuggestionsWindow(mode: mode, with: container, mentionRange: range)
+				}
 			}
 		}
-	}
 		else
 		{
 			dismissSuggestionsWindow()
@@ -168,13 +178,13 @@ open class SuggestionTextView: NSTextView
 	}
 
 	private func showSuggestionsWindow(mode: SuggestionMode,
-	                                   with suggestions: [HashtagSuggestionProtocol], mentionRange: NSRange)
+	                                   with suggestions: [SuggestionContainer], mentionRange: NSRange)
 	{
 		guard
 			mentionRange.upperBound <= (textStorage?.length ?? 0),
-			let window = self.window,
-			let layoutManager = self.layoutManager,
-			let textContainer = self.textContainer
+			let window = window,
+			let layoutManager = layoutManager,
+			let textContainer = textContainer
 		else
 		{
 			dismissSuggestionsWindow()
@@ -185,39 +195,54 @@ open class SuggestionTextView: NSTextView
 		let rect = layoutManager.boundingRect(forGlyphRange: glyphRange, in: textContainer)
 		let offsetRect = convert(rect.offsetBy(dx: textContainerInset.width, dy: textContainerInset.height), to: nil)
 		let screenRect = window.convertToScreen(offsetRect)
+		
+		switch mode {
+		case .mention:
+			activeSuggestionWindowController = accountSuggestionWindowController
+		case .hashtag:
+			activeSuggestionWindowController = hashtagSuggestionWindowController
+		}
 
-		suggestionWindowController.set(suggestions: suggestions)
-		suggestionWindowController.showWindow(nil)
+		guard let activeSuggestionWindowController else {return}
+		
+		activeSuggestionWindowController.set(suggestionContainers: suggestions)
+		activeSuggestionWindowController.showWindow(nil)
 
-		suggestionWindowController.insertSuggestionBlock =
+		activeSuggestionWindowController.insertSuggestionBlock =
+		{
+			[weak self] suggestion in
+			guard let self = self else { return }
+
+			switch suggestion
 			{
-				[weak self] suggestion in
-				guard let self = self else { return }
-				self.replaceCharacters(in: mentionRange, with: "#\(suggestion.text) ")
+			case let .mention(account):
+				self.replaceCharacters(in: mentionRange, with: "\(account.text) ")
+			case let .hashtag(hashtag):
+				self.replaceCharacters(in: mentionRange, with: "#\(hashtag.text) ")
 			}
+		}
 
-		suggestionWindowController.positionWindow(under: screenRect)
+		activeSuggestionWindowController.positionWindow(under: screenRect)
 	}
 }
 
-@objc public protocol AccountSuggestionTextViewSuggestionsProvider: AnyObject
+@objc public protocol SuggestionTextViewSuggestionsProvider: AnyObject
 {
 	func suggestionTextView(_ textView: SuggestionTextView,
 	                        suggestionsForQuery: String,
-	                        completion: @escaping ([AccountSuggestionProtocol]) -> Void)
+	                        completion: @escaping (Any) -> Void)
 }
 
-@objc public protocol HashtagSuggestionTextViewSuggestionsProvider: AnyObject
-{
-	func suggestionTextView(_ textView: SuggestionTextView,
-							suggestionsForQuery: String,
-							completion: @escaping ([HashtagSuggestionProtocol]) -> Void)
-}
-
-private enum SuggestionMode
+enum SuggestionMode
 {
 	case mention
 	case hashtag
+}
+
+enum SuggestionContainer
+{
+	case mention(AccountSuggestionProtocol)
+	case hashtag(HashtagSuggestionProtocol)
 }
 
 private extension NSString

CoreTootin/Services/AccountSearchService.swift
@@ -28,14 +28,15 @@ public class AccountSearchService
 	public init(client: ClientType, activeInstance: Instance)
 	{
 		self.client = client
-		self.instance = activeInstance
+		instance = activeInstance
 	}
 
 	public func search(query: String, completion: @escaping ([Account]) -> Void)
 	{
-		client.run(Accounts.search(query: query)) { (result) in
-
-			guard case .success(let response) = result else
+		client.run(Accounts.search(query: query))
+		{ result in
+			guard case .success(let response) = result
+			else
 			{
 				completion([])
 				return
@@ -46,38 +47,43 @@ public class AccountSearchService
 	}
 }
 
-extension AccountSearchService: AccountSuggestionTextViewSuggestionsProvider
+extension AccountSearchService: SuggestionTextViewSuggestionsProvider
 {
 	public func suggestionTextView(_ textView: SuggestionTextView,
-								   suggestionsForQuery query: String,
-								   completion: @escaping ([AccountSuggestionProtocol]) -> Void)
+	                               suggestionsForQuery query: String,
+	                               completion: @escaping (Any) -> Void)
 	{
 		let instance = self.instance
 
 		search(query: query)
 		{
-			(accounts) in
+			accounts in
 
 			DispatchQueue.main.async
-				{
-					completion(accounts.map({ AccountSuggestion(account: $0, instance: instance) }))
-				}
+			{
+				let result = accounts.map { SuggestionContainer.mention(AccountSuggestion(account: $0, instance: instance)) }
+
+				completion(result)
+			}
 		}
 	}
 }
 
-@objc public protocol AccountSuggestionProtocol {
+@objc public protocol AccountSuggestionProtocol
+{
 	var text: String { get }
 	var imageUrl: URL? { get }
 	var displayName: String { get }
 }
 
-private class AccountSuggestion : AccountSuggestionProtocol {
+private class AccountSuggestion: AccountSuggestionProtocol
+{
 	let text: String
 	let imageUrl: URL?
 	let displayName: String
 
-	init(account: Account, instance: Instance) {
+	init(account: Account, instance: Instance)
+	{
 		text = account.uri(in: instance)
 		imageUrl = account.avatarURL
 		displayName = account.bestDisplayName

CoreTootin/Services/HashtagSearchService.swift
@@ -31,16 +31,18 @@ public class HashtagSearchService {
 	}
 }
 
-extension HashtagSearchService: HashtagSuggestionTextViewSuggestionsProvider {
+extension HashtagSearchService: SuggestionTextViewSuggestionsProvider {
 	public func suggestionTextView(_ textView: SuggestionTextView,
 	                               suggestionsForQuery query: String,
-	                               completion: @escaping ([HashtagSuggestionProtocol]) -> Void)
+	                               completion: @escaping (Any) -> Void)
 	{
 		search(query: query) {
 			hashtags in
 
 			DispatchQueue.main.async {
-				completion(hashtags.map { HashtagSuggestion(hashtag: $0) })
+				let result = hashtags.map { SuggestionContainer.hashtag(HashtagSuggestion(hashtag: $0)) }
+
+				completion(result)
 			}
 		}
 	}

CoreTootin/Window Controllers/SuggestionWindowController.swift
@@ -28,21 +28,27 @@ public class SuggestionWindowController: NSWindowController
 	private var accountSuggestions: [AccountSuggestionProtocol]?
 	private var hashtagSuggestions: [HashtagSuggestionProtocol]?
 
-	public weak var imagesProvider: AccountSuggestionWindowImagesProvider? = nil
+	public weak var imagesProvider: AccountSuggestionWindowImagesProvider?
 
 	public var isWindowVisible: Bool
 	{
 		return window?.isVisible ?? false
 	}
 
-	public var insertSuggestionBlock: ((HashtagSuggestionProtocol) -> Void)? = nil
+	var insertSuggestionBlock: ((SuggestionContainer) -> Void)?
 
-	public convenience init()
+	convenience init(mode: SuggestionMode)
 	{
-		self.init(windowNibName: NSNib.Name("HashtagSuggestionWindowController"))
+		switch mode
+		{
+		case .mention:
+			self.init(windowNibName: NSNib.Name("AccountSuggestionWindowController"))
+		case .hashtag:
+			self.init(windowNibName: NSNib.Name("HashtagSuggestionWindowController"))
+		}
 	}
 
-	public override func windowDidLoad()
+	override public func windowDidLoad()
 	{
 		super.windowDidLoad()
 
@@ -52,7 +58,9 @@ public class SuggestionWindowController: NSWindowController
 
 	public func positionWindow(under textRect: NSRect)
 	{
-		if let suggestionsCount = hashtagSuggestions?.count, let tableView
+		var suggestionsCount = accountSuggestions?.count ?? hashtagSuggestions?.count
+
+		if let suggestionsCount, let tableView
 		{
 			let visibleCount = CGFloat(min(suggestionsCount, 8))
 			let bestHeight = visibleCount * (tableView.rowHeight + tableView.intercellSpacing.height)
@@ -62,9 +70,28 @@ public class SuggestionWindowController: NSWindowController
 		window?.setFrameTopLeftPoint(NSPoint(x: textRect.minX - 30, y: textRect.minY))
 	}
 
-	public func set(suggestions: [HashtagSuggestionProtocol])
+	func set(suggestionContainers: [SuggestionContainer])
 	{
-		self.hashtagSuggestions = suggestions
+		guard !suggestionContainers.isEmpty else { return }
+
+		switch suggestionContainers[0]
+		{
+		case .hashtag:
+			accountSuggestions = nil
+			hashtagSuggestions = suggestionContainers.map
+			{ if case let .hashtag(hashtag) = $0
+				{ return hashtag }
+				else { fatalError() }
+			}
+		case .mention:
+			accountSuggestions = suggestionContainers.map
+			{ if case let .mention(mention) = $0
+				{ return mention }
+				else { fatalError() }
+			}
+			hashtagSuggestions = nil
+		}
+
 		tableView?.reloadData()
 		tableView?.selectRowAndScrollToVisible(0)
 	}
@@ -74,12 +101,17 @@ public class SuggestionWindowController: NSWindowController
 		let currentSelection = tableView.selectedRow
 
 		guard
-			let suggestions = self.hashtagSuggestions,
-			(0..<suggestions.count).contains(currentSelection),
 			let block = insertSuggestionBlock
 		else { return }
 
-		block(suggestions[currentSelection])
+		if let suggestions = accountSuggestions, (0..<suggestions.count).contains(currentSelection)
+		{
+			block(SuggestionContainer.mention(suggestions[currentSelection]))
+		}
+		else if let suggestions = hashtagSuggestions, (0..<suggestions.count).contains(currentSelection)
+		{
+			block(SuggestionContainer.hashtag(suggestions[currentSelection]))
+		}
 	}
 
 	@objc func didDoubleClickTableView(_ sender: Any)
@@ -89,10 +121,11 @@ public class SuggestionWindowController: NSWindowController
 
 	@IBAction func selectNext(_ sender: Any?)
 	{
-		guard let suggestions = self.hashtagSuggestions else { return }
+		guard let suggestions = hashtagSuggestions else { return }
 		let currentSelection = tableView.selectedRow
 
-		guard (0..<suggestions.count).contains(currentSelection + 1) else
+		guard (0..<suggestions.count).contains(currentSelection + 1)
+		else
 		{
 			tableView.selectRowAndScrollToVisible(0)
 			return
@@ -103,10 +136,11 @@ public class SuggestionWindowController: NSWindowController
 
 	@IBAction func selectPrevious(_ sender: Any?)
 	{
-		guard let suggestions = self.hashtagSuggestions else { return }
+		guard let suggestions = hashtagSuggestions else { return }
 		let currentSelection = tableView.selectedRow
 
-		guard currentSelection > 0 else
+		guard currentSelection > 0
+		else
 		{
 			tableView.selectRowAndScrollToVisible(suggestions.count - 1)
 			return
@@ -120,7 +154,14 @@ extension SuggestionWindowController: NSTableViewDataSource
 {
 	public func numberOfRows(in tableView: NSTableView) -> Int
 	{
-		return hashtagSuggestions?.count ?? 0
+		if let accountSuggestions {
+			return accountSuggestions.count
+		}
+		else if let hashtagSuggestions {
+			return hashtagSuggestions.count
+		}
+		
+		return 0
 	}
 }
 
@@ -132,34 +173,34 @@ extension SuggestionWindowController: NSTableViewDelegate
 
 		let view = tableView.makeView(withIdentifier: identifier, owner: nil)
 
-//		if let suggestion = suggestions?[row], let cellView = view as? NSTableCellView
-//		{
-//			switch identifier.rawValue
-//			{
-//			case "avatar":
-//				cellView.imageView?.image = #imageLiteral(resourceName: "missing.png")
-//				guard let imageURL = suggestion.imageUrl else { break }
-//				imagesProvider?.suggestionWindow(self, imageForSuggestionUsingURL: imageURL)
-//					{
-//						[weak self] image in
-//						guard let image = image else { return }
-//						DispatchQueue.main.async
-//							{
-//								self?.updateImage(for: suggestion, originalIndex: row, image: image)
-//							}
-//					}
-//
-//			case "suggestion":
-//				cellView.textField?.stringValue = suggestion.text
-//
-//			case "displayName":
-//				cellView.textField?.stringValue = suggestion.displayName
-//
-//			default:
-//				break
-//			}
-//		}
-		
+		if let suggestion = accountSuggestions?[row], let cellView = view as? NSTableCellView
+		{
+			switch identifier.rawValue
+			{
+			case "avatar":
+				cellView.imageView?.image = #imageLiteral(resourceName: "missing.png")
+				guard let imageURL = suggestion.imageUrl else { break }
+				imagesProvider?.suggestionWindow(self, imageForSuggestionUsingURL: imageURL)
+				{
+					[weak self] image in
+					guard let image = image else { return }
+					DispatchQueue.main.async
+					{
+						self?.updateImage(for: suggestion, originalIndex: row, image: image)
+					}
+				}
+
+			case "suggestion":
+				cellView.textField?.stringValue = suggestion.text
+
+			case "displayName":
+				cellView.textField?.stringValue = suggestion.displayName
+
+			default:
+				break
+			}
+		}
+
 		if let suggestion = hashtagSuggestions?[row], let cellView = view as? NSTableCellView
 		{
 			switch identifier.rawValue
@@ -197,7 +238,7 @@ extension SuggestionWindowController: NSTableViewDelegate
 	private func updateImage(for suggestion: AccountSuggestionProtocol, originalIndex: Int, image: NSImage)
 	{
 		guard
-			let suggestions = self.accountSuggestions,
+			let suggestions = accountSuggestions,
 			originalIndex < suggestions.count,
 			suggestions[originalIndex].imageUrl == suggestion.imageUrl
 		else { return }
@@ -220,9 +261,9 @@ private extension NSTableView
 	}
 }
 
- @objc public protocol AccountSuggestionWindowImagesProvider: AnyObject
- {
+@objc public protocol AccountSuggestionWindowImagesProvider: AnyObject
+{
 	func suggestionWindow(_ windowController: SuggestionWindowController,
-						  imageForSuggestionUsingURL: URL,
-						  completion: @escaping (NSImage?) -> Void)
- }
+	                      imageForSuggestionUsingURL: URL,
+	                      completion: @escaping (NSImage?) -> Void)
+}

Mastonaut.xcodeproj/project.pbxproj
@@ -500,7 +500,7 @@
 		15F9B63F21DA7CE7008D95FE /* MastodonKit+Shims.swift in Sources */ = {isa = PBXBuildFile; fileRef = 15F9B63E21DA7CE7008D95FE /* MastodonKit+Shims.swift */; };
 		15F9F307232EB8BE00931594 /* BaseComposerTextView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 15F9F305232EB8B300931594 /* BaseComposerTextView.swift */; };
 		15F9F309232EB94F00931594 /* SuggestionTextView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 1530429222AEFCC900489F31 /* SuggestionTextView.swift */; };
-		15F9F30B232EB9E400931594 /* AccountSuggestionWindowController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 1530428022AEC64E00489F31 /* AccountSuggestionWindowController.swift */; };
+		15F9F30B232EB9E400931594 /* SuggestionWindowController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 1530428022AEC64E00489F31 /* SuggestionWindowController.swift */; };
 		15F9F30C232EB9E900931594 /* AccountSuggestionWindowController.xib in Resources */ = {isa = PBXBuildFile; fileRef = 1530428122AEC64E00489F31 /* AccountSuggestionWindowController.xib */; };
 		15FD4C33223870B000D699EF /* TransientWindow.swift in Sources */ = {isa = PBXBuildFile; fileRef = 15FD4C32223870B000D699EF /* TransientWindow.swift */; };
 		15FD4C34223870B000D699EF /* TransientWindow.swift in Sources */ = {isa = PBXBuildFile; fileRef = 15FD4C32223870B000D699EF /* TransientWindow.swift */; };
@@ -714,7 +714,7 @@
 		1530427422AE83E300489F31 /* PostingService.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = PostingService.swift; sourceTree = "<group>"; };
 		1530427722AE8F4300489F31 /* ResolverService.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = ResolverService.swift; sourceTree = "<group>"; };
 		1530427A22AE91EA00489F31 /* NSProgressIndicator+Additions.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = "NSProgressIndicator+Additions.swift"; sourceTree = "<group>"; };
-		1530428022AEC64E00489F31 /* AccountSuggestionWindowController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = AccountSuggestionWindowController.swift; sourceTree = "<group>"; };
+		1530428022AEC64E00489F31 /* SuggestionWindowController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SuggestionWindowController.swift; sourceTree = "<group>"; };
 		1530428122AEC64E00489F31 /* AccountSuggestionWindowController.xib */ = {isa = PBXFileReference; lastKnownFileType = file.xib; path = AccountSuggestionWindowController.xib; sourceTree = "<group>"; };
 		1530428C22AEDAFA00489F31 /* AccountSearchService.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = AccountSearchService.swift; sourceTree = "<group>"; };
 		1530428F22AEF5A100489F31 /* InfoWindow.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = InfoWindow.swift; sourceTree = "<group>"; };
@@ -1802,9 +1802,9 @@
 		15F9F30A232EB9D100931594 /* Window Controllers */ = {
 			isa = PBXGroup;
 			children = (
-				5FC21D0029CA511300A582A8 /* HashtagSuggestionWindowController.xib */,
-				1530428022AEC64E00489F31 /* AccountSuggestionWindowController.swift */,
 				1530428122AEC64E00489F31 /* AccountSuggestionWindowController.xib */,
+				5FC21D0029CA511300A582A8 /* HashtagSuggestionWindowController.xib */,
+				1530428022AEC64E00489F31 /* SuggestionWindowController.swift */,
 			);
 			path = "Window Controllers";
 			sourceTree = "<group>";
@@ -2689,7 +2689,7 @@
 				15431298232EF5530010854A /* AccountAccessToken.swift in Sources */,
 				159C2170232C574B00139BD5 /* NSString+Additions.swift in Sources */,
 				1543127D232EE34A0010854A /* AccountSearchService.swift in Sources */,
-				15F9F30B232EB9E400931594 /* AccountSuggestionWindowController.swift in Sources */,
+				15F9F30B232EB9E400931594 /* SuggestionWindowController.swift in Sources */,
 				15F9F309232EB94F00931594 /* SuggestionTextView.swift in Sources */,
 				1543127F232EE39D0010854A /* Account+Shims.swift in Sources */,
 				15EE2B04232EA1CC00155E16 /* Structs+Additions.swift in Sources */,

Mastonaut/Controls/ComposerTextView.swift
@@ -53,7 +53,7 @@ class ComposerTextView: BaseComposerTextView, NSTextStorageDelegate
 
 	override func insertTab(_ sender: Any?)
 	{
-		guard suggestionWindowController.isWindowVisible == false else
+		guard let activeSuggestionWindowController, activeSuggestionWindowController.isWindowVisible == false else
 		{
 			insertCurrentlySelectedSuggestion()
 			return

Mastonaut/Networking/ResourcesFetcher.swift
@@ -178,23 +178,23 @@ class ResourcesFetcher
 	}
 }
 
-//extension ResourcesFetcher: SuggestionWindowImagesProvider
-//{
-//	func suggestionWindow(_ windowController: SuggestionWindowController,
-//						  imageForSuggestionUsingURL imageURL: URL,
-//						  completion: @escaping (NSImage?) -> Void)
-//	{
-//		fetchImage(with: imageURL)
-//			{
-//				(result) in
-//
-//				guard case .success(let image) = result else
-//				{
-//					completion(nil)
-//					return
-//				}
-//
-//				completion(image)
-//			}
-//	}
-//}
+extension ResourcesFetcher: AccountSuggestionWindowImagesProvider
+{
+	func suggestionWindow(_ windowController: SuggestionWindowController,
+						  imageForSuggestionUsingURL imageURL: URL,
+						  completion: @escaping (NSImage?) -> Void)
+	{
+		fetchImage(with: imageURL)
+			{
+				(result) in
+
+				guard case .success(let image) = result else
+				{
+					completion(nil)
+					return
+				}
+
+				completion(image)
+			}
+	}
+}

commit 36d6fa4ef7c3062b8a4aee4397ed3e6c0b2e24a4

fix #103 (regression from 508289bb45e675220919412ab5d67aa22532623b): could no longer add linebreak with `return` in status composer if no autocomplete had ever been invoked

CoreTootin/Controls/BaseComposerTextView.swift
@@ -33,7 +33,7 @@ open class BaseComposerTextView: SuggestionTextView
 
 	public override func insertNewline(_ sender: Any?)
 	{
-		guard let activeSuggestionWindowController, activeSuggestionWindowController.isWindowVisible == false else
+		if activeSuggestionWindowController?.isWindowVisible == true
 		{
 			insertCurrentlySelectedSuggestion()
 			return