5930881649db8d3faefdfec2e0bba52621d275ab
Sören Kuklau <kuklau@trenz.de>
2023-11-11 00:52:41 +0100

    URL tests: add another test case, via #169

--- MastonautTests/URLSanitizerTests.swift
+++ MastonautTests/URLSanitizerTests.swift
@@ -15,7 +15,8 @@ class URLSanitizerTests: XCTestCase {
         ("https://www.dw.com/en/germany-settle-for-a-draw-despite-second-half-leroy-sané-show/a-47993172?maca=en-rss-en-all-1573-rdf", "https://www.dw.com/en/germany-settle-for-a-draw-despite-second-half-leroy-san%C3%A9-show/a-47993172?maca=en-rss-en-all-1573-rdf"),
         ("https://www.apple.com:443", nil),
         ("https://web.archive.org/web/20190213201804/http://shirky.com/writings/situated_software.html", nil),
-        ("https://mjtsai.com/blog/2023/06/06/xcode-15-announced/#xcode-15-announced-update-2023-08-23", nil)
+        ("https://mjtsai.com/blog/2023/06/06/xcode-15-announced/#xcode-15-announced-update-2023-08-23", nil),
+        ("https://forum.support.xerox.com/t5/Printing/Are-the-1200-2400-DPI-physical-specs-for-the-VersaLink-C7000/m-p/223894/highlight/true#M18964", nil)
     ]
 
     func testInputDoesNotGetMangled() {
